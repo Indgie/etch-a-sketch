638acca2784690098c795cf983ea53c24a7dcd50
Indgie <124298620+Indgie@users.noreply.github.com>
2023-08-03 10:57:17 +0300

    Complete project

--- javaSC.js
+++ javaSC.js
@@ -6,7 +6,7 @@ output.innerHTML = slider.value + " x " + slider.value; // Display the default s
 // Update the current slider value (each time you drag the slider handle)
 function sliderValueChange() {
   output.innerHTML = this.value + " x " + this.value;
-  userInput = parseInt(this.value);
+  userInput = parseInt(this.value); 
   updateGrid(userInput);
 }
 
@@ -62,57 +62,4 @@ const selectedButton = document.querySelector("#reset");
 selectedButton.addEventListener('click', () => {
     updateGrid(userInput);
   });
-
-
-
-
-
-
-//____________OLD___CODE_________//
-
-
-/*//-----SLIDER-----
-let slider = document.getElementById("myRange");
-let output = document.getElementById("sliderValue");
-output.innerHTML = slider.value + " x " + slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-function sliderValueChange() {
-  output.innerHTML = this.value + " x " + this.value;
-    userInput = parseInt(this.value);
-}
-slider.oninput = sliderValueChange;
-
-
-//-----GRID------
-let userInput = 10;//prompt("Enter number")
-const gridContainer = document.querySelector(".grid-container");
-
-for (let j = 0; j < userInput; j++) {
-    const gridbox = document.createElement("div");
-    gridContainer.appendChild(gridbox);
-    gridbox.classList.toggle("grid-box");
-    for (let i = 0; i < userInput; i++) {
-        const grid = document.createElement("div");
-        grid.classList.toggle("grid");
-        gridbox.appendChild(grid);
-}
-}
-//----COLORING----
-const colorPicked = function (e) {
-    const colorPicker = document.getElementById("colorpicker");
-    const selectedColor = colorPicker.value;
-    e.target.style.background = selectedColor;
-
-}
-const randomizedColor = function(e) {
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    e.target.style.background = "#" + randomColor;
-    color.innerHTML = "#" + randomColor;
-  }
-
-const colorGrid = document.querySelectorAll(".grid");
-Array.from(colorGrid).forEach((g) => {
-    g.addEventListener("mouseover", randomizedColor);
-});*/
 
